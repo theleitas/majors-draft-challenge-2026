c8097a67ea20fff275aa51b8bf25b476da1a63ac
Jayme Leita <theleitas@Jaymes-MacBook-Pro.local>
2026-05-14 19:51:29 -0400

    Remove test button

--- app.py
+++ app.py
@@ -814,9 +814,6 @@ st.markdown(
 )
 st.caption("**May 14–17, 2026** • Aronimink Golf Club")
 
-if st.button("Test Button"):
-    st.write("Hello Jayme")
-
 st.subheader("Standings")
 
 for coach_id, info in teams_data.items():
